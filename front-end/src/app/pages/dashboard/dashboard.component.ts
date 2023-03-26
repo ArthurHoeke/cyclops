f5f8f95f0e54688c57cebe7a560b51cbf8415b02
Arthur Hoeke <34348870+ArthurHoeke@users.noreply.github.com>
2023-03-26 21:02:59 +0200

    cleaned up graph code, added era point chart

--- front-end/src/app/pages/dashboard/dashboard.component.ts
+++ front-end/src/app/pages/dashboard/dashboard.component.ts
@@ -111,51 +111,6 @@ export class DashboardComponent {
   dailyIncomeChartType: ChartType = 'bar';
   lineChartType: ChartType = 'line';
 
-  public incomePieData: ChartData<'doughnut', number[], string | string[]> = {
-    labels: ['Amsterdam', 'Dublin', 'Paris', 'Test'],
-    datasets: [{
-      data: [300, 500, 100],
-      backgroundColor: [
-        '#5CBD0C',
-        '#F7C217',
-        '#FF5072',
-        '#A7E2FD'
-      ],
-      borderAlign: 'center',
-      borderRadius: 100,
-      borderWidth: 0,
-      spacing: 10,
-    }]
-  };
-
-  public dailyIncomeData: ChartData<'bar', number[], string | string[]> = {
-    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    datasets: [{
-      data: [15.23, 28, 12.1, 30, 11, 23, 0],
-      backgroundColor: [
-        '#14151B'
-      ],
-      borderColor: [
-        '#202127'
-      ],
-      borderWidth: 0,
-      borderRadius: 5,
-      // barPercentage: 0.5,
-      barThickness: 30,
-      maxBarThickness: 30,
-      minBarLength: 2,
-      gradient: {
-        backgroundColor: {
-          axis: 'y',
-          colors: {
-            0: 'rgba(230,0,122,.2)',
-            100: 'rgba(230,0,122,1)'
-          }
-        },
-      },
-    }]
-  };
-
   incomeChartOption: any = {
     cutout: '90%',
     radius: '80%',
@@ -214,37 +169,6 @@ export class DashboardComponent {
     }
   }
 
-
-
-
-  public lineChartData: ChartData<'line', number[], string | string[]> = {
-    labels: ['2/22', '2/23', '2/24', '2/25', '2/26', '2/27', '2/28'],
-    datasets: [{
-      data: [5000, 5100, 5312, 5681, 5923, 6123, 6421, 6734],
-      borderWidth: 2,
-      pointRadius: 2,
-      tension: 0.35,
-      gradient: {
-        backgroundColor: {
-          axis: 'y',
-          colors: {
-            0: 'rgba(230,0,122,.2)',
-            10000: 'rgba(230,0,122,.6)'
-          }
-        },
-        borderColor: {
-          axis: 'x',
-          colors: {
-            1: 'rgba(230,0,122,1)',
-          }
-        }
-      },
-      pointBorderColor: "white",
-      fill: true,
-    }],
-
-  };
-
   lineChartOptions: any = {
     responsive: false,
     maintainAspectRatio: true,
@@ -276,36 +200,6 @@ export class DashboardComponent {
     }
   }
 
-
-
-
-  public tokenChartData: ChartData<'line', number[], string | string[]> = {
-    labels: this.coingeckoService.getTestLabels(60),
-    datasets: [{
-      data: this.coingeckoService.getTestData(6, 12, 60),
-      borderWidth: 3,
-      pointRadius: 0,
-      tension: 0.25,
-      gradient: {
-        backgroundColor: {
-          axis: 'y',
-          colors: {
-            0: 'rgba(93,212,37,0)',
-            10000: 'rgba(93,212,37,1)'
-          }
-        },
-        borderColor: {
-          axis: 'x',
-          colors: {
-            1: '#68C813',
-          }
-        }
-      },
-      pointBorderColor: "white",
-      fill: true,
-    }],
-  };
-
   tokenChartOptions: any = {
     responsive: false,
     maintainAspectRatio: true,
@@ -336,34 +230,6 @@ export class DashboardComponent {
       }
     }
   }
-
-
-  public eraChartData: ChartData<'line', number[], string | string[]> = {
-    labels: ['19:10', '19:15', '19:20', '19:25', '19:30', '19:35', '19:40'],
-    datasets: [{
-      data: [0, 20, 40, 80, 160, 240, 500],
-      borderWidth: 2,
-      pointRadius: 2,
-      tension: 0.35,
-      gradient: {
-        backgroundColor: {
-          axis: 'y',
-          colors: {
-            0: 'rgba(255,238,122,0)',
-            10000: 'rgba(239,157,0,1)'
-          }
-        },
-        borderColor: {
-          axis: 'x',
-          colors: {
-            1: '#FFC300',
-          }
-        }
-      },
-      pointBorderColor: "white",
-      fill: true,
-    }],
-  };
 
   eraChartOptions: any = {
     responsive: false,
--- front-end/src/app/services/dashboard/dashboard.service.ts
+++ front-end/src/app/services/dashboard/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api/api.service';
+import { CoingeckoService } from '../coingecko/coingecko.service';
 
 import { ChartData, ChartType, Chart } from 'chart.js';
 
@@ -11,6 +12,7 @@ export class DashboardService {
   // Number variable that keeps track of the selected validator and used for panel switching
   // index 0 = overview
   private selectedValidator: Number = 0;
+  private selectedValidatorActive = false;
   private syncing: Boolean = false;
 
   public validatorList: any = [];
@@ -19,7 +21,9 @@ export class DashboardService {
   public pastEraPercentage = 50;
   public leftEraPercentage = 50;
 
-  constructor(private apiService: ApiService) { }
+  public eraRewardPoints = [];
+
+  constructor(private apiService: ApiService, private coingeckoService: CoingeckoService) { }
 
   public updateDashboardData() {
     this.updateNetworkList();
@@ -32,6 +36,10 @@ export class DashboardService {
 
   public isSyncing() {
     return this.syncing;
+  }
+
+  public isActive() {
+    return this.selectedValidatorActive;
   }
 
   public async updateValidatorList() {
@@ -52,10 +60,16 @@ export class DashboardService {
 
   private getSelectedValidatorData(valIndex: number) {
     const selVal = this.validatorList[valIndex - 1];
-    const selNetwork = this.networkList[valIndex - 1];
+    const selNetwork = this.networkList[selVal.networkId - 1];
     
-    const past = (selNetwork['era']['eraProcess'] / selNetwork['era']['eraLength']) * 100;
-    this.updateEraProgress(past, 100 - past);
+    if(selVal['details']['status'] == "active") {
+      this.selectedValidatorActive = true;
+      const past = (selNetwork['era']['eraProcess'] / selNetwork['era']['eraLength']) * 100;
+      this.updateEraProgress(past, 100 - past);
+      this.updateEraRewardPoints(selVal['details']['rewardTracking']);
+    } else {
+      this.selectedValidatorActive = false;
+    }
   }
 
   public selectValidator(index: number) {
@@ -90,6 +104,23 @@ export class DashboardService {
     });
   }
 
+  private updateEraRewardPoints(data: any) {
+    this.eraRewardPoints = data;
+
+    this.eraChartData.labels = this.createEmptyLabelList(data);
+    this.eraChartData.datasets.forEach((dataset) => {
+      dataset.data = data;
+    });
+  }
+
+  private createEmptyLabelList(input: any) {
+    let output = [];
+    for(let i = 0; i < input.length; i++) {
+      output.push("");
+    }
+    return output;
+  }
+
   public eraProgressData: ChartData<'doughnut', number[], string | string[]> = {
     labels: ['Past', 'Left'],
     datasets: [{
@@ -103,6 +134,133 @@ export class DashboardService {
       borderWidth: 0,
       spacing: 0,
 
+    }]
+  };
+
+  public eraChartData: ChartData<'line', number[], string | string[]> = {
+    labels: [],
+    datasets: [{
+      data: this.eraRewardPoints,
+      borderWidth: 2,
+      pointRadius: 2,
+      tension: 0.35,
+      gradient: {
+        backgroundColor: {
+          axis: 'y',
+          colors: {
+            0: 'rgba(255,238,122,0)',
+            100000: 'rgba(239,157,0,1)'
+          }
+        },
+        borderColor: {
+          axis: 'x',
+          colors: {
+            1: '#FFC300',
+          }
+        }
+      },
+      pointBorderColor: "white",
+      fill: true,
+    }],
+  };
+
+  public tokenChartData: ChartData<'line', number[], string | string[]> = {
+    labels: this.coingeckoService.getTestLabels(60),
+    datasets: [{
+      data: this.coingeckoService.getTestData(6, 12, 60),
+      borderWidth: 3,
+      pointRadius: 0,
+      tension: 0.25,
+      gradient: {
+        backgroundColor: {
+          axis: 'y',
+          colors: {
+            0: 'rgba(93,212,37,0)',
+            10000: 'rgba(93,212,37,1)'
+          }
+        },
+        borderColor: {
+          axis: 'x',
+          colors: {
+            1: '#68C813',
+          }
+        }
+      },
+      pointBorderColor: "white",
+      fill: true,
+    }],
+  };
+
+  public lineChartData: ChartData<'line', number[], string | string[]> = {
+    labels: ['2/22', '2/23', '2/24', '2/25', '2/26', '2/27', '2/28'],
+    datasets: [{
+      data: [5000, 5100, 5312, 5681, 5923, 6123, 6421, 6734],
+      borderWidth: 2,
+      pointRadius: 2,
+      tension: 0.35,
+      gradient: {
+        backgroundColor: {
+          axis: 'y',
+          colors: {
+            0: 'rgba(230,0,122,.2)',
+            10000: 'rgba(230,0,122,.6)'
+          }
+        },
+        borderColor: {
+          axis: 'x',
+          colors: {
+            1: 'rgba(230,0,122,1)',
+          }
+        }
+      },
+      pointBorderColor: "white",
+      fill: true,
+    }],
+
+  };
+
+  public incomePieData: ChartData<'doughnut', number[], string | string[]> = {
+    labels: ['Amsterdam', 'Dublin', 'Paris', 'Test'],
+    datasets: [{
+      data: [300, 500, 100],
+      backgroundColor: [
+        '#5CBD0C',
+        '#F7C217',
+        '#FF5072',
+        '#A7E2FD'
+      ],
+      borderAlign: 'center',
+      borderRadius: 100,
+      borderWidth: 0,
+      spacing: 10,
+    }]
+  };
+
+  public dailyIncomeData: ChartData<'bar', number[], string | string[]> = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [{
+      data: [15.23, 28, 12.1, 30, 11, 23, 0],
+      backgroundColor: [
+        '#14151B'
+      ],
+      borderColor: [
+        '#202127'
+      ],
+      borderWidth: 0,
+      borderRadius: 5,
+      // barPercentage: 0.5,
+      barThickness: 30,
+      maxBarThickness: 30,
+      minBarLength: 2,
+      gradient: {
+        backgroundColor: {
+          axis: 'y',
+          colors: {
+            0: 'rgba(230,0,122,.2)',
+            100: 'rgba(230,0,122,1)'
+          }
+        },
+      },
     }]
   };
 }
